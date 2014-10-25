b2ab0343ee9d530372a3fff03f85036c72502a78
Kayodan <anthony.caillaud@gmail.com>
2014-10-25 13:19:14 +0200

    behavior and css

---
+++ AngularApp/behaviors/directives.js
@@ -0,0 +1,15 @@
+myApp.directive("enter", function(){
+	return function(scope, element, attrs){
+		element.bind("mouseenter", function(){
+			element.addClass(attrs.enter);
+		});
+	}
+})
+
+myApp.directive("leave", function(){
+	return function(scope, element, attrs){
+		element.bind("mouseleave", function(){
+			element.removeClass(attrs.enter);
+		});
+	}
+})
